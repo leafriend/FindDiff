cc01177d958559fa73f991c9ef17f03f0d142e38
leafriend <leafriend@gmail.com>
2014-06-27 11:42:31 +0900

    * Print file name and progress during comparing.   * Compare file content also -- before, it compared only file size.

--- src/main/java/net/folab/finddiff/FindDiff.java
+++ src/main/java/net/folab/finddiff/FindDiff.java
@@ -2,6 +2,7 @@ package net.folab.finddiff;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.ArrayList;
@@ -80,8 +81,84 @@ public class FindDiff {
     }
 
     public static void findFileDiff(File lFile, File rFile, String path) {
+        String head;
+        if (lFile.isFile() && rFile.isFile() && "".equals(path)) {
+            head = "~   " + lFile.getName();
+        } else {
+            head = "~   " + path;
+        }
+        System.out.print(head);
         if (lFile.length() != rFile.length()) {
-                System.out.println("*   " + path);
+            clear(head, false);
+            System.out.println("*   " + path);
+            return;
+        }
+        FileInputStream lin = null;
+        FileInputStream rin = null;
+        byte[] lbuf = new byte[1024 * 1024];
+        byte[] rbuf = new byte[1024 * 1024];
+        int llen;
+        int rlen;
+        String stat;
+        long length = lFile.length();
+        long offset = 0;
+        int w = String.valueOf(length).length();
+        w = w + ((w - 1) / 3);
+        try {
+            lin = new FileInputStream(lFile);
+            rin = new FileInputStream(rFile);
+            stat = ": " + String.format("%6.2f", ((double) offset * 100) / length) + "% (" + String.format("%," + w + "d", offset) + "/" + String.format("%," + w + "d", length) + ")";
+            while ((llen = lin.read(lbuf)) > 0) {
+                offset += llen;
+                System.out.print(stat);
+                if ((rlen = rin.read(rbuf)) != llen) {
+                    // TODO handle rest bytes
+                    throw new RuntimeException(llen + " != " + rlen);
+                }
+                for (int i = 0; i < llen; i++) {
+                    if (lbuf[i] != rbuf[i]) {
+                        clear(head, false);
+                        System.out.println("*   " + path);
+                        return;
+                    }
+                }
+                clear(stat, false);
+                stat = ": " + String.format("%6.2f", ((double) offset * 100) / length) + "% (" + String.format("%," + w + "d", offset) + "/" + String.format("%," + w + "d", length) + ")";
+            }
+            System.out.print(stat);
+            clear(stat, true);
+            clear(head, true);
+        } catch (IOException e) {
+            clear(head, false);
+            System.out.println("!   " + path);
+            e.printStackTrace();
+        } finally {
+            if (lin != null) {
+                try {
+                    lin.close();
+                } catch (IOException e) {
+                }
+            }
+            if (rin != null) {
+                try {
+                    rin.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+    }
+
+    public static void clear(String path, boolean whitening) {
+        for (int i = 0; i < path.length(); i++) {
+            System.out.print("\b");
+        }
+        if (whitening) {
+            for (int i = 0; i < path.length(); i++) {
+                System.out.print(" ");
+            }
+            for (int i = 0; i < path.length(); i++) {
+                System.out.print("\b");
+            }
         }
     }
 
